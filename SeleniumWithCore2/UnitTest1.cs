2293d21f61cdd21b5005941548304961dcf8e9bb
Nicholas Reed <nreed@fusionalliance.com>
2018-05-22 13:24:57 -0400

    Selenium 3.12 (WebDriver, Support), Selenium Chrome WebDriver (2.38.0), MSTest (TestAdapter, TestFramework) 1.2.1, in a .NET Core 2.0 project running in VS 2017 (15.7.1)

--- SeleniumWithCore2/UnitTest1.cs
+++ SeleniumWithCore2/UnitTest1.cs
@@ -1,13 +1,52 @@
+using System;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
 
 namespace SeleniumWithCore2
 {
     [TestClass]
-    public class UnitTest1
+    public class SeleniumWebTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        IWebDriver driver;
+        string driverDirectory;
+        [TestInitialize]
+        public void TestInitialize()
         {
+            this.driverDirectory = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            this.driver = new ChromeDriver(driverDirectory);
         }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            this.driver.Close();
+        }
+
+        [TestMethod]
+        public void FailingTest()
+        {
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+            driver.FindElement(By.Name("UserName")).SendKeys("admin");
+            driver.FindElement(By.Name("Password")).SendKeys("admin");
+            driver.FindElement(By.Name("Login")).Submit();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(drv => drv.Url.Equals("http://executeautomation.com/demosite/index.html?UserName=admin&Password=adminNOT"));            
+        }
+
+        [TestMethod]
+        public void PassingTest()
+        {
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+            driver.FindElement(By.Name("UserName")).SendKeys("admin");
+            driver.FindElement(By.Name("Password")).SendKeys("admin");
+            driver.FindElement(By.Name("Login")).Submit();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(drv => drv.Url.Equals("http://executeautomation.com/demosite/index.html?UserName=admin&Password=admin"));
+        }
+
     }
 }
