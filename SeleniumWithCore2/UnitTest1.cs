d1badcca1c31fc4d14959821a904943e4a02eb54
Nicholas Reed <nreed@fusionalliance.com>
2018-05-22 10:53:34 -0400

    Add project files.

---
+++ SeleniumWithCore2/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumWithCore2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
